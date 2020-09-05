5ee19b83905abdddf6cf2f2da482fc6d92697a98
Leifeon001 <tristen.howk@yahoo.com>
2020-09-05 12:26:11 -0400

    Assets and Scripts
    Created Testing scripts for Resources and Harvestors

---
+++ CityBuilder/Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Player Stats")]
+    public float WoodMod;
+    public float StoneMod;
+    public float FishMod;
+    public float Harvesters;
+    [Header("Player Resources")]
+    public int Wood;
+    public int Stone;
+    public int Fish;
+
+    public bool TurnEnded;
+    int TempMod;
+    public void EndTurn()
+    {
+        TurnEnded = true;
+        foreach (HarvestPoint HPoint in FindObjectsOfType<HarvestPoint>())
+        {
+            if (HPoint.Harvested == true)
+            {
+                HPoint.Results();
+                HPoint.Harvested = false;
+                TurnEnded = false;
+                Harvesters++;
+            }
+        }
+    }
+
+    public void AddRes(int Res, int Amount)
+    {
+        switch (Res)
+        {
+            case 0:
+                Wood += Amount;
+                break;
+            case 1:
+                Stone += Amount;
+                break;
+            case 2:
+                Fish += Amount;
+                break;
+        }
+    }
+    public int Modifiers(int WRes, int BGain)
+    {
+        switch (WRes)
+        {
+            case 0:
+                TempMod = Mathf.RoundToInt(BGain * WoodMod);
+                break;
+            case 1:
+                TempMod = Mathf.RoundToInt(BGain * StoneMod);
+                break;
+            case 2:
+                TempMod = Mathf.RoundToInt(BGain * FishMod);
+                break;
+        }
+        Debug.Log("Value Returned: " + TempMod + " from case Value: "+ WRes);
+        return TempMod;
+    }
+}
---
+++ CityBuilder/Assets/Scripts/ResScripts/Resource.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Resource", menuName = "New Resource")]
+public class Resource : ScriptableObject
+{
+    public string Name;
+    public int MinAmount;
+    public int MaxAmount;
+}

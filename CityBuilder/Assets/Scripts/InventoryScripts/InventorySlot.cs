a45c5fabbaab8a879081f9c554b62fa76b2e058b
KishoreBoopathi <60489284+KishoreBoopathi@users.noreply.github.com>
2020-09-08 09:49:57 -0400

    Inventory System

---
+++ CityBuilder/Assets/Scripts/InventoryScripts/InventorySlot.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+
+    public Image resourceIcon;
+    public Button removeButton;
+
+    Resource resource;
+
+    public void AddResource(Resource newResource)
+    {
+        resource = newResource;
+
+        resourceIcon.sprite = resource.icon;
+        resourceIcon.enabled = true;
+        removeButton.interactable = true;
+    }
+
+    public void ClearSlot ()
+    {
+        resource = null;
+
+        resourceIcon.sprite = null;
+        resourceIcon.enabled = false;
+        removeButton.interactable = false;
+    }
+
+    public void OnRemoveButton()
+    {
+        Inventory.instance.Remove(resource);
+    }
+    
+    public void UseResource ()
+    {
+        if(resource != null)
+        {
+            resource.UseResource();
+            ClearSlot();
+        }
+    }
+
+}
--- CityBuilder/Assets/Scripts/ResScripts/Resource.cs
+++ CityBuilder/Assets/Scripts/ResScripts/Resource.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "New Resource", menuName = "New Resource")]
 public class Resource : ScriptableObject
 {
     public string Name;
+    public Sprite icon;
     public int MinAmount;
     public int MaxAmount;
+
+    public virtual void UseResource ()
+    {
+        //Usage of the resource
+
+        Debug.Log("Using " + Name);
+    }
 }
+
